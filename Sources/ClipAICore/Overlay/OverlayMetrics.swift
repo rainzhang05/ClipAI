fff171183be3cc6a1915b2b548342fa81fa89864
Rain Zhang <tza103@sfu.ca>
2026-07-07 23:14:39 -0700

    Clip overlay card corners to fix shadow artifacts

--- Sources/ClipAICore/Overlay/OverlayMetrics.swift
+++ Sources/ClipAICore/Overlay/OverlayMetrics.swift
@@ -2,9 +2,10 @@ import Foundation
 
 enum OverlayMetrics {
     static let panelWidth: CGFloat = 480
+    static let cardCornerRadius: CGFloat = 20
     static let initialPanelSize = CGSize(width: 460, height: 200)
     static let maxContentHeight: CGFloat = 300
-    static let toastHeight: CGFloat = 34
+    static let copyButtonHeight: CGFloat = 34
     static let screenMargin: CGFloat = 16
     static let animationDuration: TimeInterval = 0.3
     static let contentReplaceDuration: TimeInterval = 0.15
